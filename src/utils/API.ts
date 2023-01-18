622d8908eb4851df84acdf9e28bbbd8b638470d7
yangcxPusto <619980817@qq.com>
2023-01-18 08:16:15 +0000

    request to HTTPS. not work on another machine

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const base_url = process.env.REACT_APP_ENV === 'development'?'http://localhost:5000/':'http://yangchnx.com:5000/';
+const base_url = process.env.REACT_APP_ENV === 'development'?'http://localhost:5000/':'https://yangchnx.com:5000/';
 
 export default axios.create({
   baseURL: base_url,
